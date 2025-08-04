6c8802d380585d327c6db9f1a88f9967846c731e
f1zzye <kosenko2401@gmail.com>
2025-08-04 18:40:58 +0300

    add: ton connect functionality

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,6 +6,7 @@ from core.views import (
     RaffleView,
     CashbackInfoView,
     DepositView,
+    tonconnect_manifest,get_wallet_balance
 )
 
 app_name: str = "core"
@@ -16,4 +17,6 @@ urlpatterns = [
     path("raffle/", RaffleView.as_view(), name="raffle"),
     path("cashback-info/", CashbackInfoView.as_view(), name="cashback"),
     path("deposit/", DepositView.as_view(), name="deposit"),
+    path("tonconnect-manifest.json", tonconnect_manifest, name="tonconnect_manifest"),
+    path('api/wallet-balance/', get_wallet_balance, name='wallet_balance'),
 ]
