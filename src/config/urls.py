518fe5823bdf9afba56ae885f943adf5d625fc62
sauberr <sauberr10@gmail.com>
2025-08-19 19:05:50 +0300

    Add localization and fix small bugs.

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,15 +1,18 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
+    path("i18n/", include("django.conf.urls.i18n")),
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
     path("exchange/", include("exchange.urls", namespace="exchange")),
     path("user/", include("accounts.urls", namespace="account")),
-]
+    prefix_default_language=False
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
