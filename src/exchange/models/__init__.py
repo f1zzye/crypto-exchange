07aece40cc635da1639eeec0530559f5434e997c
user <kosenko2401@gmail.com>
2025-08-25 15:48:09 +0300

    new app and order model

--- src/exchange/models/__init__.py
+++ src/exchange/models/__init__.py
@@ -1,6 +1,9 @@
 from .pool import Pool
 from .token import Token
 from .network import Network
-from .exchange_order import ExchangeOrder
 
-__all__ = ["Pool", "Token", "Network", "ExchangeOrder"]
+__all__ = [
+    "Pool",
+    "Token",
+    "Network",
+]
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from common.mixins import TitleMixin
+
+from .models import ExchangeOrder
+
+
+class OrderCreateView(TitleMixin, TemplateView):
+    template_name: str = "exchange/order_success.html"
+    title: str = "Заявка создана - CryptoChicken"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        order_id = self.request.session.get("order_id")
+        if order_id:
+            try:
+                order = ExchangeOrder.objects.get(id=order_id)
+                context["order"] = order
+            except ExchangeOrder.DoesNotExist:
+                pass
+
+        return context
