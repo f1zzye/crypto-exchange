52c630a522a683493eea64d330b96ff4ce6bfff4
f1zzye <kosenko2401@gmail.com>
2025-09-10 16:50:38 +0300

    exchange transaction

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -22,11 +22,11 @@ CORS_ALLOW_HEADERS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://*.ngrok.io",
     "https://*.ngrok-free.app",
-    "https://27661817f1ed.ngrok-free.app",
+    "https://e39850f69d90.ngrok-free.app",
 ]
 
 CORS_ALLOWED_ORIGINS: list[str] = [
-    "https://27661817f1ed.ngrok-free.app",
+    "https://e39850f69d90.ngrok-free.app",
 ]
 
 DATABASES = {
